638e59e16bed7d6169e7860f33036eb731a68d4e
alexander <adkomyagin@gmail.com>
2013-11-24 00:07:36 -0500

    started implementation of server oplog fetcher

--- imongo.h
+++ imongo.h
@@ -3,3 +3,9 @@
 
 int64_t mongoread(const char *file_name, char *data);
 
+
+#define CHUNKS_NS "ctest.fs.chunks"
+#define METADATA_NS "ctest.fs.files"
+
+extern bson_timestamp_t milestone;
+int fetch_metadata(const char *server, const char *client, bson_timestamp_t start);
